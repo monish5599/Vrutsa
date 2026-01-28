0241f76e6783d0ae88f3a1ce718abad23c504b52
Monish <monish5599@gmail.com>
2026-01-28 15:59:13 +0530

    fix port for render

--- backend/index.js
+++ backend/index.js
@@ -126,6 +126,8 @@ app.get("/admin/contacts", async (req, res) => {
 });
 
 /* ================= SERVER ================= */
-app.listen(5000, () => {
-  console.log("🚀 Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
